d86eac8bc895d269929c0d4daa4a5ee824440ac1
[najib] <najibabiyyu50@gmail.com>
2026-06-15 09:53:45 +0700

    change versi laravel 12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Event;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -22,33 +18,6 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'test123',
-        ]);
-
-        User::create([
-            'name' => 'Super Admin',
-            'email' => 'admin@kampus.com',
-            'password' => Hash::make('admin123'),
-            'role' => 'admin',
-        ]);
-
-        User::create([
-            'name' => 'Mahasiswa Reguler',
-            'email' => 'mahasiswa@kampus.com',
-            'password' => Hash::make('password123'),
-            'role' => 'user',
-        ]);
-
-        Event::create([
-            'title' => 'Webinar Akselerasi Karir UI/UX Designer 2026',
-            'speaker' => 'Evan Wijaya',
-            'category' => 'UI/UX',
-            'type' => 'online',
-            'description' => 'Belajar fundamental UI/UX dari nol hingga siap kerja di industri.',
-            'location' => 'Zoom Meeting',
-            'event_date' => '2026-07-20 13:00:00',
-            'price' => 50000,
-            'ticket_quantity' => 100,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
-
-Route::middleware(['auth', 'verified'])->group(function () {
-   Route::get('/dashboard', [EventController::class, 'index'])->name('dashboard');
-    Route::get('/event/{id}', [EventController::class, 'show'])->name('event.show');
-    Route::post('/event/{id}/book', [BookingController::class, 'store'])->name('booking.store');
-    Route::get('/my-tickets', [BookingController::class, 'index'])->name('booking.index');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// RUTE KHUSUS ADMIN
-Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('dashboard');
-
-});
-
-require __DIR__.'/auth.php';
